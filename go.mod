46b2fcc94fb4eb96e0cce954c0f4e306be6ecc74
tianbao <silas@sky-city.me>
2024-05-07 16:15:46 +0800

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/shinvdu/goproxy
+
+go 1.19
+
+require (
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5
+	github.com/elazarl/goproxy v0.0.0-20231117061959-7cc037d33fb5
+	github.com/go-httpproxy/httpproxy v0.0.0-20180417134941-6977c68bf38e
+)
+
+require golang.org/x/net v0.25.0 // indirect
---
+++ socks.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  // "log"
+  // "net/http"
+  "github.com/armon/go-socks5"
+)
+
+func main() {
+  // Create a SOCKS5 server
+  conf := &socks5.Config{}
+  server, err := socks5.New(conf)
+  if err != nil {
+    panic(err)
+  }
+
+  // Create SOCKS5 proxy on localhost port 8000
+  if err := server.ListenAndServe("tcp", "0.0.0.0:8080"); err != nil {
+    panic(err)
+  }
+}
